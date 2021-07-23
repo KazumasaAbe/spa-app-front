6ee8321457de3285353021996e893a88a8d5273c
iijima-tatsunori <himekawamaki45@gmail.com>
2021-07-24 08:13:29 +0900

    fixed_switching.vue

--- pages/switching.vue
+++ pages/switching.vue
@@ -6,24 +6,6 @@
 </template>
 
 <script>
-export default {
-  async asyncData ({ $axios }) {
-    let hostDetails = []
-    await $axios.$get('/api/v1/host_details')
-      .then(res => (hostDetails = res))
-    return { hostDetails }
-  },
-  computed: {
-    dateFormat () {
-      return (date) => {
-        const dateTimeFormat = new Intl.DateTimeFormat(
-          'ja', { dateStyle: 'medium', timeStyle: 'short' }
-        )
-        return dateTimeFormat.format(new Date(date))
-      }
-    }
-  }
-}
 </script>
 
 /* eslint-enable */
