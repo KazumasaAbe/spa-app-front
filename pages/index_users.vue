edf1df580047a1f7f2b7a3214ea900ad4b274944
AbeKazumasa <katahira0088@gmail.com>
2021-09-13 22:39:17 +0900

    UPDATE完了

--- pages/index_users.vue
+++ pages/index_users.vue
@@ -2,7 +2,7 @@
   <v-main>
     <v-container>
       <v-row justify="center" align-content="center" class="text-caption">
-        <v-col cols="8">
+        <v-col cols="10">
           <v-card>
             <v-tabs
               v-model="tab"
@@ -20,7 +20,7 @@
               <v-tab-item>
                 <v-card flat>
                   <v-data-table
-                    :headers="headers"
+                    :headers="host_headers"
                     :items="hostUsers"
                     :ites-per-page="10"
                     class="elevation-1"
@@ -46,7 +46,7 @@
               <v-tab-item>
                 <v-card flat>
                   <v-data-table
-                    :headers="headers"
+                    :headers="general_headers"
                     :items="gengeralUsers"
                     :ites-per-page="10"
                     class="elevation-1"
@@ -81,6 +81,7 @@
 
                 <v-card-text>
                   <v-container>
+                    <Notification v-if="errors" :messages="errors" />
                     <v-col
                       cols="12"
                       sm="6"
@@ -88,65 +89,97 @@
                     >
                       <v-text-field
                         v-model="editedItem.email"
+                        prepend-icon="mdi-account-circle"
                         label="メールアドレス"
                       />
                     </v-col>
+                    <span v-if="editedItem.host_name !== null">
+                      <v-col
+                        cols="12"
+                        sm="6"
+                        md="5"
+                      >
+                        <v-select
+                          v-model="editedItem.host_name"
+                          item-text="name"
+                          item-value="value"
+                          :items="details"
+                          label="店舗"
+                          prepend-icon="mdi-home-plus-outline"
+                          @change="hostSelect()"
+                        />
+                      </v-col>
+                    </span>
+                    <v-card-actions>
+                      <v-spacer />
+                      <v-btn
+                        color="blue darken-1"
+                        text
+                        @click="close()"
+                      >
+                        Cancel
+                      </v-btn>
+
+                      <v-btn
+                        color="blue darken-1"
+                        text
+                        @click="save()"
+                      >
+                        Save
+                      </v-btn>
+                    </v-card-actions>
                   </v-container>
                 </v-card-text>
               </v-card>
-              <v-card-actions>
-                <v-spacer />
-                <v-btn
-                  color="blue darken-1"
-                  text
-                  @click="close()"
-                >
-                  Cancel
-                </v-btn>
-
-                <v-btn
-                  color="blue darken-1"
-                  text
-                  @click="checkForm()"
-                >
-                  Save
-                </v-btn>
-              </v-card-actions>
             </v-dialog>
           </v-card>
         </v-col>
-      </v-row>
+      </v-row>1
     </v-container>
   </v-main>
 </template>
 
 <script>
+import Notification from '../components/Notification.vue'
 export default {
   auth: false,
+  components: { Notification },
   data () {
     return {
       dialog: false,
+      errors: null,
       dialogDelete: false,
       formTitle: '編集',
       tab: null,
+      details: [],
       users: [],
       host_users: [],
       general_users: [],
-      headers: [
+      host_headers: [
         {
           text: 'メールアドレス',
           align: 'start',
           value: 'email'
         },
         {
-          text: 'host',
+          text: '店舗名',
           align: 'start',
-          value: 'host'
+          value: 'host_name'
+        },
+        { text: '編集 / 削除', value: 'actions', sortable: false }
+      ],
+      general_headers: [
+        {
+          text: 'メールアドレス',
+          align: 'start',
+          value: 'email'
         },
         { text: '編集 / 削除', value: 'actions', sortable: false }
       ],
       editedItem: {
-        email: ''
+        email: '',
+        host_name: '',
+        host_id: ''
       },
       editedIndex: -1,
       tabsName: [
@@ -168,12 +201,11 @@ export default {
       .get('/api/v1/users.json')
       .then(response => (this.users = response.data)
       )
+    this.$axios
+      .get('/api/v1/host_details.json')
+      .then(response => (this.details = response.data))
   },
   methods: {
-    generalUsers () {
-      this.general_users = this.users.filter(user => user.host === false)
-      return this.general_users
-    },
     editItem (item) {
       this.editedIndex = this.users.indexOf(item)
       this.editedItem = Object.assign({}, item)
@@ -182,11 +214,33 @@ export default {
     deleteItem () {
       console.log(2)
     },
-    checkForm () {
-      console.log(1)
+    save () {
+      const url = '/api/v1/users/'
+      this.$axios.patch(url + this.editedItem.id, this.editedItem)
+        .then(() => {
+          Object.assign(this.users[this.editedIndex], this.editedItem)
+          this.$store.dispatch(
+            'flashMessage/showMessage',
+            {
+              message: '更新しました',
+              type: 'success',
+              status: true
+            }
+          )
+          this.close()
+        })
+        .catch((e) => {
+          console.log(e.response.data)
+          this.errors = e.response.data
+        })
     },
     close () {
       this.dialog = false
+      this.errors = null
+    },
+    hostSelect () {
+      this.detail = this.details.filter(detail => detail.name === this.editedItem.host_name)
+      this.editedItem.host_id = this.detail[0].id
     }
   }
 }
