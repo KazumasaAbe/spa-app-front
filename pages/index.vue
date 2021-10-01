9bb949c408effc409b83a45181af17326184eb4b
AbeKazumasa <katahira0088@gmail.com>
2021-10-01 22:50:14 +0900

    finish

--- pages/index.vue
+++ pages/index.vue
@@ -3,62 +3,113 @@
   <v-app id="app" class="map">
     <v-container>
       <v-row justify="center">
-        <GmapMap
-          ref="gmp"
-          :center="center"
-          :zoom="zoom"
-          style="width: 100%; height: 600px"
+        <v-col cols="12">
+          <GmapMap
+            ref="gmp"
+            :center="center"
+            :zoom="zoom"
+            style="width: 100%; height: 600px"
+          >
+            <GmapMarker
+              v-for="(m, index) in setDetails()"
+              :key="m.name"
+              :title="m.name"
+              :position="{ lat: parseFloat(m.latitude), lng: parseFloat(m.longitude) }"
+              :animation="Number(m.acceptable)"
+              :clickable="true"
+              :draggable="false"
+              :optimized="false"
+              :z-index="0"
+              :icon="{url: markerIcon2(m.marker_icon),
+                      scaledSize: { width: 80, height: 80, f: 'px', b: 'px' },
+                      anchor: { x:25, y:18}}"
+              @click="onClickMarker(index, m)"
+            />
+            <GmapMarker
+              v-for="(m, index) in setDetails()"
+              :key="index"
+              :title="m.name"
+              :position="{ lat: parseFloat(m.latitude), lng: parseFloat(m.longitude) }"
+              :animation="Number(m.acceptable)"
+              :clickable="true"
+              :draggable="false"
+              :optimized="false"
+              :z-index="1"
+              :icon="{url: markerIcon(m.marker_icon),
+                      scaledSize: { width: 30, height: 30, f: 'px', b: 'px' },
+                      anchor: { x:0, y:0}}"
+              @click="onClickMarker(index, m)"
+            />
+            <GmapInfoWindow
+              :options="infoOptions"
+              :position="infoWindowPos"
+              :opened="infoWinOpen"
+              @closeclick="infoWinOpen = false"
+            >
+              <p style="color: #000">
+                {{ marker.name }}
+              </p>
+            </GmapInfoWindow>
+          </GmapMap>
+        </v-col>
+      </v-row>
+      <v-row justify="center">
+        <v-btn-toggle
+          v-model="toggle_exclusive"
+          color="primary"
+          multiple
         >
-          <GmapMarker
-            v-for="(m, index) in hostDetails"
-            :key="index"
-            :title="m.name"
-            :position="{ lat: parseFloat(m.latitude), lng: parseFloat(m.longitude) }"
-            :animation="Number(m.acceptable)"
-            :clickable="true"
-            :draggable="false"
-            :icon="{url: markerIcon(m.marker_icon),
-                    scaledSize: { width: 30, height: 30, f: 'px', b: 'px' }}"
-            @click="onClickMarker(index, m)"
-          />
-          <GmapInfoWindow
-            :options="infoOptions"
-            :position="infoWindowPos"
-            :opened="infoWinOpen"
-            @closeclick="infoWinOpen = false"
+          <v-col cols="12">
+            <v-btn
+              v-for="(tag, i) in tagsIndex"
+              :key="i"
+              class="ma-1"
+              color="#001F47"
+              dark
+              @click="
+                activeCheck(tag, i)
+              "
+            >
+              {{ tag.tag }}
+            </v-btn>
+          </v-col>
+        </v-btn-toggle>
+      </v-row>
+      <v-row>
+        <v-col cols="12">
+          <ul>
+            <li
+              v-for="(name, i) in selectTags"
+              :key="i"
+            >
+              {{ name.tag }}
+            </li>
+          </ul>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12">
+          <v-data-table
+            :headers="headers"
+            :items="setDetails()"
+            sort-by="name"
+            class="elevation-1"
           >
-            <p style="color: #000">
-              {{ marker.name }}
-            </p>
-          </GmapInfoWindow>
-        </GmapMap>
-        <v-row>
-          <v-btn-toggle
-            v-model="toggle_exclusive"
-            color="primary"
-            multiple
-          >
-            <v-col cols="12">
+            <template #[`item.tags`]="{ item }">
               <v-btn
-                v-for="(tag, i) in tags"
+                v-for="(tag,i) in item.tags"
                 :key="i"
                 class="ma-1"
-                color="success"
-                @click="
-                  activeCheck(tag, i)
-                "
+                sm
+                disabled
               >
                 {{ tag.tag }}
               </v-btn>
-            </v-col>
-          </v-btn-toggle>
-          <span>{{ toggle_exclusive }}</span>
-          <v-btn
-            @click="selectTag()"
-          >
-            check
-          </v-btn>
-        </v-row>
+            </template>
+          </v-data-table>
+        </v-col>
+      </v-row>
+      <v-row>
         <v-dialog
           v-model="dialog"
           max-width="800px"
@@ -125,18 +176,20 @@ export default {
   auth: false,
   data () {
     return {
-      anyTags: {},
-      manyTags: [],
-      tag_name: '',
-      selectDetails: [],
+      finish_host: [],
+      finish: [],
+      result: [],
+      serch: [],
+      sample: [],
       selectTags: [],
+      tagsAll: [],
+      tagsIndex: [],
       toggle_exclusive: [],
       tag_index: {},
       selectedTemplate: false,
       selectedItem: false,
       color: null,
       dialog: false,
-      tags: {},
       marker: {},
       hostDetails: [],
       selecetDetails: [],
@@ -154,7 +207,19 @@ export default {
           width: 0,
           height: -35
         }
-      }
+      },
+      headers: [
+        {
+          text: '地図上に表示されているお店名',
+          align: 'start',
+          sortable: false,
+          value: 'name'
+        },
+        {
+          text: 'タグ',
+          value: 'tags'
+        }
+      ]
     }
   },
   computed: {
@@ -162,16 +227,22 @@ export default {
       user: 'user_information/getUser'
     })
   },
+  watch: {
+    toggle_exclusive (val, old) {
+      this.selectTag()
+    }
+  },
   mounted () {
     this.$axios
       .get('/api/v1/host_details.json')
       .then(response => (this.hostDetails = response.data)
       )
-    this.serchDetails = this.hostDetails
     this.$axios
       .get('/api/v1/tags')
-      .then(response => (this.tags = response.data)
-      )
+      .then((res) => {
+        this.tagsAll = res.data.tags
+        this.tagsIndex = res.data.select_tags
+      })
   },
   methods: {
     onClickMarker (index, marker) {
@@ -191,6 +262,9 @@ export default {
         return i
       }
     },
+    markerIcon2 (i) {
+      return '/img/bloon1.png'
+    },
     activeCheck (tag, i) {
       this.selectedTemplate = tag
       this.selectedIndex = i
@@ -203,35 +277,56 @@ export default {
       this.toggle_exclusive.forEach((val) => {
         this.method2(val)
       })
+      this.method3()
     },
     method2 (i) {
-      this.selectTags.push(this.tags[i])
-      console.log(this.selectTags)
-      this.method4()
-    },
-    method4 () {
-      if (this.toggle_exclusive.length === 0) {
-        this.serchDetails = this.hostDetails
-      } else {
-        this.serchDetails = []
-        this.selectTags.forEach((item) => {
-          this.tag_name = item
-          this.method3()
-        })
-      }
+      this.selectTags.push(this.tagsIndex[i])
     },
     method3 () {
-      this.hostDetails.forEach((item) => {
-        this.manyTags = item.tags
-        this.anyTags = this.manyTags[0]
-        console.log(this.anyTags)
-        if (item.tags.includes(this.tag_name)) {
-          this.serchDetails.push(this.item)
-          console.log(this.serchDetails)
-        } else {
-          console.log(false)
-        }
+      this.serch = []
+      this.selectTags.forEach((item) => {
+        this.sample = this.tagsAll.filter(tag => tag.tag === item.tag)
+        this.method5()
       })
+      this.method6()
+      this.method7()
+    },
+    method5 () {
+      if (this.sample) {
+        this.sample.forEach((item) => {
+          this.serch.push(item)
+        })
+      } else {
+        console.log(5)
+      }
+    },
+    method6 () {
+      this.result = this.serch.filter((element, index, self) =>
+        self.findIndex(e => e.host_detail_id === element.host_detail_id) === index
+      )
+    },
+    method7 () {
+      this.finish_host = []
+      this.result.forEach((item) => {
+        this.finish = this.hostDetails.filter(h => h.id === item.host_detail_id)
+        this.method8()
+      })
+    },
+    method8 () {
+      if (this.finish[0]) {
+        this.finish_host.push(this.finish[0])
+      } else {
+        console.log(6)
+      }
+    },
+    setDetails () {
+      if (this.finish_host[0]) {
+        console.log(1)
+        return this.finish_host
+      } else {
+        console.log(2)
+        return this.hostDetails
+      }
     }
   }
 }
